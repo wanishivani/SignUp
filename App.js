a0ab95167c79028872c843b5cb759bf43025b1f2
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-05-25 14:16:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./Store/AuthContext";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        {!authCtx.IsLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/profile">
+          {authCtx.IsLoggedIn && <UserProfile />}
+          {!authCtx.IsLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
